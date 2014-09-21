9eda6e9b92d48f61c881e17f776042b5965ac18e
Louis Houghton <Louis@Midgar.local>
2014-09-21 14:48:20 +0100

    Moved key out into ignored file

--- src/lib/init.js
+++ src/lib/init.js
@@ -3,7 +3,7 @@
 	Initialise the Instagram API and authenticate.
 */
 var instagramApi = Instajam.init({
-    clientId: '4f9f3a58b64c47fb9f0f9ec8a94b1ea3',
+    clientId: instagramKey,
     redirectUri: 'http://localhost:8080',
     scope: ['basic', 'comments']
 });
