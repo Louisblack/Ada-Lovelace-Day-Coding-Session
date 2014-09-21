d244f6c1efbf8d19e0086577c271487bafa00840
Louis Houghton <Louis@Midgar.local>
2014-09-21 14:54:54 +0100

    Switched tabs for spaces

--- src/lib/init.js
+++ src/lib/init.js
@@ -1,6 +1,6 @@
 
 /*
-	Initialise the Instagram API and authenticate.
+    Initialise the Instagram API and authenticate.
 */
 var instagramApi = Instajam.init({
     clientId: instagramKey,
@@ -11,35 +11,35 @@ var instagramApi = Instajam.init({
 if (!instagramApi.authenticated) {
         window.location = API.authUrl;
 } else {
-	console.log("Logged in");
-	console.log(instagramApi);
+    console.log("Logged in");
+    console.log(instagramApi);
 }
 
 var map = new google.maps.Map(document.getElementById("map"), {
-	zoom: 6
+    zoom: 6
 });
 
 var addPhotoToPage = function(photo) {
-	var newImage = $('<img src="' + photo + '"/>' );
-	$('#photos').append(newImage);
+    var newImage = $('<img src="' + photo + '"/>' );
+    $('#photos').append(newImage);
 }
 
 var addPhotoToMap = function(photo, lat, long) {
-	$('#map').removeClass('hidden');
-	var myLatlng = new google.maps.LatLng(lat, long);
+    $('#map').removeClass('hidden');
+    var myLatlng = new google.maps.LatLng(lat, long);
 
-	var marker = new google.maps.Marker({
-		position: myLatlng,
-		map: map
-	});
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+    });
 
-	google.maps.event.addListener(marker, 'mouseover', function() {
-		var image = $("#image");
-		image.removeClass('hidden');
-		image.attr('src', photo);
-  	});
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        var image = $("#image");
+        image.removeClass('hidden');
+        image.attr('src', photo);
+      });
 }
 
 $(document).ready(function() {
-	loadPhotos();
+    loadPhotos();
 });
